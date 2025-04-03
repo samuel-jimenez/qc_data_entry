f285664f03a01b40b4661760ed8afd9f9191b0ea
Samuel Jimenez <you@example.com>
2025-04-03 14:06:38 -0500

    go fmt

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 import "github.com/tadvi/winc"
+
 // https://pkg.go.dev/github.com/tadvi/winc
 
 func main() {
@@ -10,11 +11,11 @@ func main() {
 }
 func show_window() {
 
-    fmt.Println("Hello, World!")
-    // DEBUG
+	fmt.Println("Hello, World!")
+	// DEBUG
 
 	mainWindow := winc.NewForm(nil)
-	mainWindow.SetSize(400, 300)  // (width, height)
+	mainWindow.SetSize(400, 300) // (width, height)
 	mainWindow.SetText("Hello World Demo")
 	show_water_based(mainWindow)
 }
@@ -27,8 +28,7 @@ func show_water_based(mainWindow *winc.Form) {
 	submit_row := 80
 	group_row := 120
 
-
-// type 
+	// type
 	visual_label := winc.NewLabel(mainWindow)
 	visual_label.SetPos(label_col, visual_row)
 
@@ -48,8 +48,7 @@ func show_water_based(mainWindow *winc.Form) {
 	group_field := winc.NewGroupBox(mainWindow)
 	group_field.SetPos(field_col, group_row)
 
-
-	 edit_label := winc.NewLabel(mainWindow)
+	edit_label := winc.NewLabel(mainWindow)
 	edit_label.SetPos(label_col, edit_row)
 
 	edit_box := winc.NewEdit(mainWindow)
@@ -59,8 +58,8 @@ func show_water_based(mainWindow *winc.Form) {
 
 	btn := winc.NewPushButton(mainWindow)
 	btn.SetText("Show or Hide")
-	btn.SetPos(40, submit_row)	// (x, y)
-	btn.SetSize(100, 40) // (width, height)
+	btn.SetPos(40, submit_row) // (x, y)
+	btn.SetSize(100, 40)       // (width, height)
 	btn.OnClick().Bind(func(e *winc.Event) {
 		if edit_box.Visible() {
 			edit_box.Hide()
@@ -78,9 +77,8 @@ func show_water_based(mainWindow *winc.Form) {
 
 func show_fr(mainWindow *winc.Form) {
 
-    fmt.Println("Hello, World!")
-    // DEBUG
-
+	fmt.Println("Hello, World!")
+	// DEBUG
 
 	edit_label := winc.NewEdit(mainWindow)
 	edit_label.SetText("Edit")
@@ -92,7 +90,7 @@ func show_fr(mainWindow *winc.Form) {
 
 	btn := winc.NewPushButton(mainWindow)
 	btn.SetText("Show or Hide")
-	btn.SetPos(40, 50)	// (x, y)
+	btn.SetPos(40, 50)   // (x, y)
 	btn.SetSize(100, 40) // (width, height)
 	btn.OnClick().Bind(func(e *winc.Event) {
 		if edit_box.Visible() {
@@ -108,7 +106,6 @@ func show_fr(mainWindow *winc.Form) {
 
 	winc.RunMainLoop() // Must call to start event loop.
 }
-
 
 func wndOnClose(arg *winc.Event) {
 	winc.Exit()
