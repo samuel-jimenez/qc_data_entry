bf9603db9d6837a1939b6a505469b4b4beffea8c
samuel-jimenez <53528911+samuel-jimenez@users.noreply.github.com>
2025-04-14 08:47:31 -0500

    sqllite

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 var SAMPLE_VOLUME = 83.2
 var LB_PER_GAL = 8.345 // g/mL
 var LABEL_PATH = "C:/Users/QC Lab/Documents/golang/qc_data_entry/labels"
-var DB_PATH = "C:/Users/QC Lab/Documents/golang/qc_data_entry/qc.db"
+var DB_PATH = "C:/Users/QC Lab/Documents/golang/qc_data_entry/qc.sqlite3"
 
 var qc_db *sql.DB
 var db_get_product, db_insert_product,
@@ -221,9 +221,9 @@ func dbinit(db *sql.DB) {
 
 	sqlStmt := `
 PRAGMA foreign_keys = ON;
-create table product_line (product_id integer not null primary key, product_name text unique);
-create table product_lot (lot_id integer not null primary key, lot_name text, product_id references product, unique (lot_name,product_id));
-create table qc_samples (qc_id integer not null primary key, lot_id references product_lot, sample_point text, time_stamp integer, specific_gravity real,  ph real,   string_test real,   viscosity real);
+create table bs.product_line (product_id integer not null primary key, product_name text unique);
+create table bs.product_lot (lot_id integer not null primary key, lot_name text, product_id references product, unique (lot_name,product_id));
+create table bs.qc_samples (qc_id integer not null primary key, lot_id references product_lot, sample_point text, time_stamp integer, specific_gravity real,  ph real,   string_test real,   viscosity real);
 `
 
 	// 	sqlStmt := `
@@ -250,28 +250,28 @@ create table qc_samples (qc_id integer not null primary key, lot_id references p
 	// 	return
 	// }
 
-	get_product_statement := `select product_id from product_line where product_name = ?`
+	get_product_statement := `select product_id from bs.product_line where product_name = ?`
 	db_get_product, err = db.Prepare(get_product_statement)
 	if err != nil {
 		log.Printf("%q: %s\n", err, get_product_statement)
 		return
 	}
 
-	insert_product_statement := `insert into product_line (product_name) values (?) returning product_id`
+	insert_product_statement := `insert into bs.product_line (product_name) values (?) returning product_id`
 	db_insert_product, err = db.Prepare(insert_product_statement)
 	if err != nil {
 		log.Printf("%q: %s\n", err, insert_product_statement)
 		return
 	}
 
-	get_lot_statement := `select lot_id from product_lot join product_line using (product_id) where lot_name = ? and product_name = ?`
+	get_lot_statement := `select lot_id from bs.product_lot join product_line using (product_id) where lot_name = ? and product_name = ?`
 	db_get_lot, err = db.Prepare(get_lot_statement)
 	if err != nil {
 		log.Printf("%q: %s\n", err, get_lot_statement)
 		return
 	}
 
-	insert_lot_statement := `insert into product_lot (lot_name) values (?) returning lot_id`
+	insert_lot_statement := `insert into bs.product_lot (lot_name) values (?) returning lot_id`
 	db_insert_lot, err = db.Prepare(insert_lot_statement)
 	if err != nil {
 		log.Printf("%q: %s\n", err, insert_lot_statement)
