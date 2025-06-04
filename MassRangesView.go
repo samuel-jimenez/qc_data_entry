72de7ed352a8f7bfee53584d32414fa591d82546
samuel-jimenez <53528911+samuel-jimenez@users.noreply.github.com>
2025-06-04 08:24:50 -0500

    chore: extract MassRangesView

---
+++ MassRangesView.go
@@ -0,0 +1,64 @@
+package main
+
+import "github.com/samuel-jimenez/windigo"
+
+/* DerivedMassRangesView
+ *
+ */
+type DerivedMassRangesView struct {
+	mass_field,
+	sg_field,
+	density_field *RangeROView
+}
+
+func (view DerivedMassRangesView) CheckMass(data float64) bool {
+	return view.mass_field.Check(data)
+}
+
+func (view DerivedMassRangesView) CheckSG(data float64) bool {
+	return view.sg_field.Check(data)
+}
+
+func (view DerivedMassRangesView) CheckDensity(data float64) bool {
+	return view.density_field.Check(data)
+}
+
+func (data_view DerivedMassRangesView) Clear() {
+	data_view.mass_field.Clear()
+	data_view.sg_field.Clear()
+	data_view.density_field.Clear()
+}
+
+/* MassRangesView
+ *
+ */
+type MassRangesView interface {
+	CheckMass(data float64) bool
+	CheckSG(data float64) bool
+	CheckDensity(data float64) bool
+}
+
+/* MassDataView
+ *
+ */
+type MassDataView struct {
+	windigo.LabeledEdit
+	Clear func()
+}
+
+// type MassDataView interface {
+// 	windigo.LabeledEdit
+// 	Clear()
+// }
+
+// func (data_view MassDataView) Clear() {
+// 	data_view.mass_field.Clear()
+// 	data_view.sg_field.Clear()
+// 	data_view.density_field.Clear()
+// }
+
+// func (data_view windigo.LabeledEdit) Clear() {
+// 	data_view.mass_field.Clear()
+// 	data_view.sg_field.Clear()
+// 	data_view.density_field.Clear()
+// }
--- gui.go
+++ gui.go
@@ -237,63 +237,11 @@ func show_text(parent windigo.AutoPanel, label_width, field_width, field_height 
 	return windigo.LabeledEdit{ComponentFrame: panel, Edit: text_field}
 }
 
-type DerivedMassRangesView struct {
-	mass_field,
-	sg_field,
-	density_field *RangeROView
-}
-
-func (view DerivedMassRangesView) CheckMass(data float64) bool {
-	return view.mass_field.Check(data)
-}
-
-func (view DerivedMassRangesView) CheckSG(data float64) bool {
-	return view.sg_field.Check(data)
-}
-
-func (view DerivedMassRangesView) CheckDensity(data float64) bool {
-	return view.density_field.Check(data)
-}
-
-type MassRangesView interface {
-	CheckMass(data float64) bool
-	CheckSG(data float64) bool
-	CheckDensity(data float64) bool
-}
-
-func (data_view DerivedMassRangesView) Clear() {
-	data_view.mass_field.Clear()
-	data_view.sg_field.Clear()
-	data_view.density_field.Clear()
-}
-
-type MassDataView struct {
-	windigo.LabeledEdit
-	Clear func()
-}
-
 // TODO View
 func clear_field(field windigo.LabeledEdit) {
 	field.SetText("")
 	field.SetBorder(okPen)
 }
-
-// type MassDataView interface {
-// 	windigo.LabeledEdit
-// 	Clear()
-// }
-
-// func (data_view MassDataView) Clear() {
-// 	data_view.mass_field.Clear()
-// 	data_view.sg_field.Clear()
-// 	data_view.density_field.Clear()
-// }
-
-// func (data_view windigo.LabeledEdit) Clear() {
-// 	data_view.mass_field.Clear()
-// 	data_view.sg_field.Clear()
-// 	data_view.density_field.Clear()
-// }
 
 // func show_mass_sg(parent windigo.AutoPanel, label_width, control_width, height int, field_text string, ranges_panel MassRangesView) windigo.LabeledEdit {
 func show_mass_sg(parent windigo.AutoPanel, label_width, control_width, height int, field_text string, ranges_panel MassRangesView) MassDataView {
