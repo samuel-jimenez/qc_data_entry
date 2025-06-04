99fb5f447c290cf0c10512ad2d04f8350431ead5
samuel-jimenez <53528911+samuel-jimenez@users.noreply.github.com>
2025-06-04 11:35:39 -0500

    chore: expand NumberEditableView

--- NumberEditView.go
+++ NumberEditView.go
@@ -14,7 +14,9 @@ import (
 type NumberEditableView interface {
 	windigo.LabeledEdit
 	Get() float64
+	GetFixed() float64
 	Clear()
+	Check(bool)
 }
 
 /*
@@ -26,12 +28,12 @@ type NumberEditView struct {
 	*windigo.Edit
 }
 
-func (control NumberEditView) Get() float64 {
+func (control *NumberEditView) Get() float64 {
 	val, _ := strconv.ParseFloat(strings.TrimSpace(control.Text()), 64)
 	return val
 }
 
-func (control NumberEditView) GetFixed() float64 {
+func (control *NumberEditView) GetFixed() float64 {
 	start, end := control.Selected()
 	// IndexAny(s, chars string) int
 	control.SetText(strings.TrimSpace(control.Text()))
@@ -42,12 +44,12 @@ func (control NumberEditView) GetFixed() float64 {
 	return control.Get()
 }
 
-func (control NumberEditView) Clear() {
+func (control *NumberEditView) Clear() {
 	control.SetText("")
 	control.Ok()
 }
 
-func (control NumberEditView) Check(test bool) {
+func (control *NumberEditView) Check(test bool) {
 	if test {
 		control.Ok()
 	} else {
@@ -72,7 +74,6 @@ func NewNumberEditViewWithChange(parent windigo.Controller, label_width, control
 		edit_field.Check(range_field.Check(edit_field.GetFixed()))
 	})
 	return edit_field
-
 }
 
 func NewNumberEditViewWithUnits(parent windigo.AutoPanel, label_width, field_width, field_height int, field_text, field_units string) *NumberEditView {
