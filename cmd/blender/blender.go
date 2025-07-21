acdfec30ca81935550fd8d0cbab7bb7387ce8228
samuel-jimenez <53528911+samuel-jimenez@users.noreply.github.com>
2025-07-21 11:07:58 -0500

    gui product

--- cmd/blender/blender.go
+++ cmd/blender/blender.go
@@ -6,12 +6,52 @@ import (
 	"os"
 
 	"github.com/samuel-jimenez/qc_data_entry/DB"
+	"github.com/samuel-jimenez/qc_data_entry/GUI"
 	"github.com/samuel-jimenez/qc_data_entry/config"
 	"github.com/samuel-jimenez/qc_data_entry/threads"
 	"github.com/samuel-jimenez/windigo"
 
 	_ "github.com/ncruces/go-sqlite3/driver"
 	_ "github.com/ncruces/go-sqlite3/embed"
+)
+
+var (
+	WINDOW_WIDTH,
+	WINDOW_HEIGHT,
+	WINDOW_FUDGE_MARGIN,
+
+	TOP_SPACER_WIDTH,
+	TOP_SPACER_HEIGHT,
+	INTER_SPACER_HEIGHT,
+	BTM_SPACER_WIDTH,
+	BTM_SPACER_HEIGHT,
+
+	TOP_PANEL_WIDTH,
+	REPRINT_BUTTON_WIDTH,
+	TOP_PANEL_INTER_SPACER_WIDTH,
+
+	RANGE_WIDTH,
+
+	PRODUCT_FIELD_WIDTH,
+	PRODUCT_FIELD_HEIGHT,
+	DATA_FIELD_WIDTH,
+	DATA_SUBFIELD_WIDTH,
+	DATA_UNIT_WIDTH,
+	DATA_MARGIN,
+	FIELD_HEIGHT,
+	NUM_FIELDS,
+
+	RANGES_RO_PADDING,
+	RANGES_RO_FIELD_WIDTH,
+	RANGES_RO_SPACER_WIDTH,
+
+	BUTTON_WIDTH,
+	BUTTON_HEIGHT,
+	BUTTON_MARGIN,
+
+	GROUP_WIDTH,
+	GROUP_HEIGHT,
+	GROUP_MARGIN int
 )
 
 var (
@@ -67,11 +107,21 @@ func dbinit(db *sql.DB) {
 func show_window() {
 
 	log.Println("Info: Process started")
+
 	window_title := "QC Data Blender"
 
+	product_text := "Product"
 	// build window
 	mainWindow := windigo.NewForm(nil)
 	mainWindow.SetText(window_title)
+
+	dock := windigo.NewSimpleDock(mainWindow)
+
+	product_field := GUI.NewComboBox(mainWindow, product_text)
+
+	// product_field := GUI.NewSizedListComboBox(prod_panel, label_width, field_width, field_height, product_text)
+
+	dock.Dock(product_field, windigo.Top)
 
 	mainWindow.Center()
 	mainWindow.Show()
